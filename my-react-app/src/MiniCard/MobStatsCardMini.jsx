dd372d24f01e98ed5465c67c5c96113834dd8eb7
WongYC-66 <yengchien@hotmal.com>
2024-02-02 02:46:21 +0800

    add tested mobstats

--- my-react-app/src/MiniCard/MobStatsCardMini.jsx
+++ my-react-app/src/MiniCard/MobStatsCardMini.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import Card from 'react-bootstrap/Card';
+// 
+import { mobIdToMobStats, decodeElemAttr } from "../myUtility.js"
 
 function MobStatsCardMini({ data, isTarget }) {
     const [mobData, setMobData] = useState({})
@@ -7,54 +9,29 @@ function MobStatsCardMini({ data, isTarget }) {
     // console.log(isTarget)
 
     useEffect(() => {
-        const fetchMobData = async () => {
-            const fetchParameter = [
-                ['GMS', 64],
-                ['GMS', 82],
-                ['GMS', 107],
-                ['EMS', 55],
-                ['EMS', 82],
-                ['KMST', 1087],
-                ['CMST', 172],
-                ['CMS', 148],
-                ['SEA', 198]
-            ]
-            // fetch api for eqp data for at least 3 times if fail
-            for (let i = 0; i < fetchParameter.length; i++) {
-                let x = {};
-                try {
-                    x = await fetch(`https://maplestory.io/api/${fetchParameter[i][0]}/${fetchParameter[i][1]}/mob/${data.id}`)
-                    x = await (x.json())
-                    // return setEqpData(x)
-                    let nextObj = {
-                        name: x.name,
-                        id: x.id,
-                        level: x.meta.level,
-                        maxHP: x.meta.maxHP,
-                        maxMP: x.meta.maxMP,
-                        WDEF: x.meta.physicalDefense,
-                        WDMG: x.meta.physicalDamage,
-                        MDMG: x.meta.magicDefense,
-                        MDEF: x.meta.magicDamage,
-                        accuracy: x.meta.accuracy,
-                        exp: x.meta.exp,
-                        isUndead: x.meta.isUndead,
-                        knockBack: x.meta.minimumPushDamage,
-                        version: `${fetchParameter[i][0]}  v${fetchParameter[i][1]}`,
-                    }
-                    // console.log(nextObj)
-                    setMobData(nextObj)
-                    setIsLoading(false)
-                    // console.log(`${fetchParameter[i][0]}  ${fetchParameter[i][1]}`)
-                    return;  // no error then end.
-                } catch (err) {
-                    setMobData({})
-                    continue // re-fetch if error
-                }
-
+        const fetchMobData = () => {
+            let x = mobIdToMobStats(data.id)
+            let nextObj = {
+                level: x.level || null,
+                maxHP: x.maxHP || null,
+                maxMP: x.maxMP || null,
+                WDEF: x.PDDamage || null,
+                WDMG: x.PADamage || null,
+                MDMG: x.MADamage || null,
+                MDEF: x.MDDamage || null,
+                accuracy: x.acc || null,
+                evasion: x.eva || null,
+                exp: x.exp || null,
+                isUndead: x.undead === "1",
+                isBoss: x.boss === "1",
+                knockBack: x.pushed || null,
+                elemAttr: x.elemAttr || ""
             }
-            setIsLoading(false) // loading fail. done with fetch.
+            if (x.elemAttr) nextObj.elemAttr = decodeElemAttr(x.elemAttr)
+            setIsLoading(false)
+            setMobData(nextObj)
         }
+
         fetchMobData()
     }, [data])
 
--- my-react-app/src/MobStatsCard/MobStatsCard.jsx
+++ my-react-app/src/MobStatsCard/MobStatsCard.jsx
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 // 
+import { mobIdToMobStats, decodeElemAttr } from "../myUtility.js"
 import styles from './MobStatsCard.module.css'
 
 function MobStatsCard({ data }) {
@@ -10,55 +11,29 @@ function MobStatsCard({ data }) {
     const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
-        const fetchMobData = async () => {
-            const fetchParameter = [
-                ['GMS', 64],
-                ['GMS', 82],
-                ['GMS', 107],
-                ['EMS', 55],
-                ['EMS', 82],
-                ['KMST', 1087],
-                ['CMST', 172],
-                ['CMS', 148],
-                ['SEA', 198]
-            ]
-            // fetch api for eqp data for at least 3 times if fail
-            for (let i = 0; i < fetchParameter.length; i++) {
-                let x = {};
-                try {
-                    x = await fetch(`https://maplestory.io/api/${fetchParameter[i][0]}/${fetchParameter[i][1]}/mob/${data.id}`)
-                    x = await (x.json())
-                    // return setEqpData(x)
-                    let nextObj = {
-                        name: x.name,
-                        id: x.id,
-                        level: x.meta.level,
-                        maxHP: x.meta.maxHP,
-                        maxMP: x.meta.maxMP,
-                        WDEF: x.meta.physicalDefense,
-                        WDMG: x.meta.physicalDamage,
-                        MDMG: x.meta.magicDefense,
-                        MDEF: x.meta.magicDamage,
-                        accuracy: x.meta.accuracy,
-                        exp: x.meta.exp,
-                        isUndead: x.meta.isUndead,
-                        isBoss: x.meta.isBoss || false,
-                        knockBack: x.meta.minimumPushDamage,
-                        version: `${fetchParameter[i][0]}  v${fetchParameter[i][1]}`,
-                    }
-                    // console.log(nextObj)
-                    setMobData(nextObj)
-                    setIsLoading(false)
-                    // console.log(`${fetchParameter[i][0]}  ${fetchParameter[i][1]}`)
-                    return;  // no error then end.
-                } catch (err) {
-                    setMobData({})
-                    continue // re-fetch if error
-                }
-
+        const fetchMobData = () => {
+            let x = mobIdToMobStats(data.id)
+            let nextObj = {
+                level: x.level || null,
+                maxHP: x.maxHP || null,
+                maxMP: x.maxMP || null,
+                WDEF: x.PDDamage || null,
+                WDMG: x.PADamage || null,
+                MDMG: x.MADamage || null,
+                MDEF: x.MDDamage || null,
+                accuracy: x.acc || null,
+                evasion: x.eva || null,
+                exp: x.exp || null,
+                isUndead: x.undead === "1",
+                isBoss: x.boss === "1",
+                knockBack: x.pushed || null,
+                elemAttr: x.elemAttr || ""
             }
-            setIsLoading(false) // loading fail. done with fetch.
+            if (x.elemAttr) nextObj.elemAttr = decodeElemAttr(x.elemAttr)
+            setIsLoading(false)
+            setMobData(nextObj)
         }
+
         fetchMobData()
     }, [data])
 
@@ -67,7 +42,7 @@ function MobStatsCard({ data }) {
     return (
         <>
             {isLoading ? <span className={`${styles.loader} d-block text-center mx-auto`}></span>
-                : (typeof mobData.level !== "number") ? <span>No Info</span>
+                : (typeof mobData.level !== "string") ? <span>No Info</span>
                     :
                     <div className='stats'>
                         {mobData.isBoss && <h1 className="h-6 fs-2 text-danger text-center">BOSS</h1>}
@@ -88,10 +63,15 @@ function MobStatsCard({ data }) {
                                 <Col>{mobData.isUndead ? "Yes" : "No"}</Col>
                             </Row>
                             <Row>
+                                <Col xs={5}>Exp </Col>
+                                <Col xs={1}>:</Col>
+                                <Col>{numToString(mobData.exp * 3.2)}</Col>
+                            </Row>
+                            {/* <Row>
                                 <Col xs={5}>Info-version</Col>
                                 <Col xs={1}>:</Col>
                                 <Col>{mobData.version}</Col>
-                            </Row>
+                            </Row> */}
                         </Container>
 
                     </div>
